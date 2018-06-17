5bbcf2ec67b7a564b91d734581bef5b4680f6b21
fsc <1694336389@qq.com>
2018-06-17 18:20:53 +0800

    添加关于的fragment

---
+++ app/src/main/java/com/fsc/newsnets/about/widget/AboutFragment.java
@@ -0,0 +1,19 @@
+package com.fsc.newsnets.about.widget;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.fsc.newsnets.R;
+
+/**关于fragment
+ * Created by fsc on 2018/6/17.
+ */
+
+public class AboutFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle saveInstanceState ){
+        View view = inflater.inflate(R.layout.fragment_about,null);
+        return view;
+    }
+}
