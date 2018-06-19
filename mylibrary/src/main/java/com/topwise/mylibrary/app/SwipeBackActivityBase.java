04ccda4796434044c71c458bfb4b342b2e0af0db
fanshengcai <fanshengcai@topwisesz.com>
2018-06-19 19:50:22 +0800

    给项目添加一个lib资源库

---
+++ mylibrary/src/main/java/com/topwise/mylibrary/app/SwipeBackActivityBase.java
@@ -0,0 +1,21 @@
+package com.topwise.mylibrary.app;
+
+import com.topwise.mylibrary.SwipeBackLayout;
+
+/**
+ * @author fsc
+ */
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    public abstract SwipeBackLayout getSwipeBackLayout();
+
+    public abstract void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    public abstract void scrollToFinishActivity();
+
+}
